d72c849f01fbe45352be8f512bb6b5e7c1e6e4f2
Joseph-King93 <j.king61693@gmail.com>
2023-02-01 02:45:55 -0500

    initial testing/functionality for pokeapi request/response calls

--- pokeapp/views.py
+++ pokeapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-
+def testing(request):
+    print("button worked")
+    r = requests.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+    data = json.loads(r.content)
+    print(r.status_code)
+    print(data['name'])
+    return render(request, 'home.html')
 
